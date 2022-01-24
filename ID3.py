7caa0286860962ae973c4d03bb728eaadc5c65d9
aka-ccc <ra8919ratw@gmail.com>
2022-01-24 14:09:47 +0800

    0124
    結論只有兩種的基本上應該都OKOK了
    兩種以上的晚點改晚點改QQ

--- ID3.py
+++ ID3.py
@@ -1,16 +1,16 @@
 #coding=utf-8
-import operator
-from tkinter.tix import Tree
+from attr import attributes
 import pandas as pd
-import random
-
-# df=pd.read_csv("./觀測天氣之資料表.csv")
-# df=df.sort_values("天氣",ascending=True)
-# test=df[['氣溫','天氣','濕度','風','結論']]
-# print(df.head(15))
+import math
 
 
-# hello
+
+class clean_data:
+    clean_source=pd.DataFrame()
+    def __init__(self,dataframe):
+        self.clean_source=dataframe
+        pass
+
 
 
 class clean_data:
@@ -21,8 +21,9 @@ class clean_data:
 
 
 class raw_data:
-    conclusions=[] #結論
-    raw_source=pd.DataFrame()  #把csv讀成pandas的dataFrame 
+    class_info = 0.0  #類別訊息獲取量
+    attributes = {}  #建字典對到 effect attribute
+    conclusions = [] #結論
     
     class_info=0.0  #類別訊息獲取量
     attributes={}  #建字典對到 effect attribute
@@ -117,16 +118,34 @@ class raw_data:
         print('印reorder前的\n',self.raw_source.head()) #印reorder前的
 
     def read_in_csv(): #讀csv順便把attributes字典建立，記得用pandas讀csv
-        pass
-    
-    
+        df = pd.read_csv("./觀測天氣之資料表.csv") #讀檔
 
+        con = df['結論'].sort_values()
+        data_sum = con.count() #資料總數
+        con_type = con.T.drop_duplicates() #結論有哪幾種
+        for result in con_type: #把結論存起來
+            raw_data.conclusions.append(result)
+        con_type_num = len(raw_data.conclusions)
+# 計算class_info
+        for count in range(0,con_type_num):
+            upper = con.value_counts()[count]
+            raw_data.class_info -= (upper/data_sum)*math.log2(upper/data_sum)
+        
+        for col in df.columns: #取得全部屬性
+            if col != "結論":
+                raw_data.attributes[col] = effect_attribute(name = col, con = raw_data.conclusions, gainA = raw_data.class_info)
+
+        print(raw_data.class_info) #印出類別資訊量for check check!
+        return df, raw_data.attributes #回傳csv資料
+
+#公式 >>> [-= type/all(log2(type/all))]
+    
 class effect_attribute:
-    effect_attr_name=""    #屬性名稱
-    attr_info=0.0  #屬性訊息量
-    gainA=0.0   #屬性訊息獲取量
-    conclusions=[]   #結論
-    attr_subset={}  #屬性對應結論
+    effect_attr_name = ""    #屬性名稱
+    attr_info = 0.0  #屬性訊息量
+    gainA = 0.0   #屬性訊息獲取量
+    conclusions = []   #結論
+    attr_subset = {}  #屬性對應結論
     '''
     attr_subset={}
     conclusions=["不好","好"]
@@ -146,20 +165,55 @@ class effect_attribute:
     "雨天" : [0,2]}
 
     '''
-    def __init__(self,name):
-        self.effect_attr_name=name
-        self.fake_data()
+    def __init__(self, name, con, gainA):
+        self.effect_attr_name = name
+        self.conclusions = con
+        self.gainA = gainA
+        
     
-    #測試用
-    def fake_data(self):
-        self.attr_info=random.random()
-        self.gainA=random.random()
+    def caculate_attr(self, data):
         
+        data_sum = len(data) #資料總筆數
+        attr_data = data[self.effect_attr_name] #屬性資料
+        data_type = attr_data.drop_duplicates() #取得屬性資料的種類
+        attr_con = data.value_counts() #屬性+結論的個別總數
         
-def main():
-    test=raw_data(file_path="./觀測天氣之資料表.csv")
-    pass
+        data_type_num = data[self.effect_attr_name].value_counts() #屬性資料的個別總數 （有 9 無 0)
 
-if __name__ == "__main__":
-    # pass
-    main()
+        
+#這裡看起來會出大事>>>>就如果說 屬性資料用一樣的敘述好像會蓋掉資料ㄇ 還是其實沒啥差<<他反正我現在看起來是存在同一個dic裡面啦@_@
+        for type in data_type: #初始化屬性結論
+            temp_info = 0.0
+            data_upper = data_type_num[type]
+            con = self.conclusions
+            #先假設只有兩ㄍ結論我之後再改QQ
+            type_con0 = attr_con[type].get(con[0]) #該屬性的性質的結論結果
+            type_con1 = attr_con[type].get(con[1])
+
+            #修正無資料ㄉ東東
+            if type_con0 == None:
+                type_con0 = 0
+            if type_con1 == None:
+                type_con1 = 0
+
+            type_sum = type_con0 + type_con1 #之後改之後改
+
+            self.attr_subset[type] = [type_con0,type_con1]
+            for result in self.attr_subset[type]:
+                if result == 0:
+                    break
+                else:
+                    temp_info -= (result/type_sum)*math.log2(result/type_sum)
+            self.attr_info += (data_upper/data_sum)*temp_info
+
+        # print(self.attr_subset)
+        print(self.effect_attr_name)
+        return self.attr_info
+
+csv_df, attr = raw_data.read_in_csv()
+for sub_attr in raw_data.attributes:
+
+    arrange_sub_attr_data = csv_df[[sub_attr, '結論']].sort_values(by = '結論').sort_values(by = sub_attr) #排序過後的資料 > 只取該屬性跟結論
+    print(attr[sub_attr].caculate_attr(arrange_sub_attr_data))
+    # print(arrange_sub_attr_data)
+# print(csv_df)
